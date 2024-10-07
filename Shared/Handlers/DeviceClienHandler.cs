411f88cb4d34112ccffb96ee2597dcaa4549d26d
Dessezar <Cristoffer.Dessezar@hotmail.com>
2024-10-07 20:18:13 +0200

    Add project files.

---
+++ Shared/Handlers/DeviceClienHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Shared.Models;
+
+namespace Shared.Handlers;
+
+public class DeviceClienHandler
+{
+    private readonly DeviceSettings _settings = new();
+    private DeviceClient? _client;
+    public DeviceClienHandler(string deviceId, string deviceType, string deviceName)
+    {
+        _settings!.DeviceId = deviceId;
+        _settings.DeviceType = deviceType;
+        _settings.DeviceName = deviceName;
+    }
+
+    public void Initialize()
+    {
+        _client = DeviceClient.CreateFromConnectionString(_settings.ConnectionString);
+    }
+}
---
+++ Shared/Models/DeviceSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Models;
+
+public class DeviceSettings
+{
+    public string DeviceId { get; set; } = null!;
+    public string? DeviceName { get; set;}
+    public string? DeviceType { get; set; }
+    public bool DeviceState { get; set; }
+
+    public bool ConnectionsState { get; set; }
+    public string ConnectionString { get; set; } = null!;
+}
