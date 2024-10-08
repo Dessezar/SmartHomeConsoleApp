d03518f32ad8ba4d0150122ffd29520479bac822
Cristoffer Dessezar <Cristoffer.Dessezar@hotmail.com>
2024-10-08 19:30:31 +0200

    adding deviceID to azure, but must create the azure hub first.

--- SmartHomeConsoleApp/Program.cs
+++ SmartHomeConsoleApp/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Shared.Handlers;
 
-var deviceClientHandler = new DeviceClientHandler("", "", "");
+
+
+
+
+var deviceClientHandler = new DeviceClientHandler("86f55ef4-bb5c-4b9f-9fe2-b4ad27599b5d", "console app", "console");
 var result = await deviceClientHandler.InitializeAsync();
 
 Console.WriteLine(result.Message);
